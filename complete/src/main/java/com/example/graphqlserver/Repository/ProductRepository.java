0b5d51f9b20056830df77d2e2bd0c4089affb26a
euler <eullerborgesdamotta155@gmail.com>
2025-04-02 16:33:04 -0300

    looking for product with name done

--- complete/src/main/java/com/example/graphqlserver/Repository/ProductRepository.java
+++ complete/src/main/java/com/example/graphqlserver/Repository/ProductRepository.java
@@ -3,5 +3,9 @@ package com.example.graphqlserver.Repository;
 import com.example.graphqlserver.Entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    List<ProductEntity> findByNameContainingIgnoreCase(String name);
 }
